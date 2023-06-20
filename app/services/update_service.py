943031d2480d9fcaec64a5412728ce04cc17a415
shahid.sayyed <shahid.sayyed@alixpartners.com>
2023-06-20 14:43:59 +0530

    removed timer

--- app/services/update_service.py
+++ app/services/update_service.py
@@ -14,16 +14,6 @@ logger.setLevel(gunicorn_logger.level)
 
 load_dotenv()
 
-def timer_func(func):
-    # This function shows the execution time of
-    # the function object passed
-    def wrap_func(*args, **kwargs):
-        t1 = time()
-        result = func(*args, **kwargs)
-        t2 = time()
-        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
-        return result
-    return wrap_func
 
 def set_env_var():
     global sqlUserName,sqlPassword,sqlDatabaseName,sqlServerName,sqlSchemaName
