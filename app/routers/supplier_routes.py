cc8b36480209949b21b24b070a3ea76143d4a6ad
shahid.sayyed <shahid.sayyed@alixpartners.com>
2023-06-20 14:43:38 +0530

    new route for get-supplier-info

--- app/routers/supplier_routes.py
+++ app/routers/supplier_routes.py
@@ -47,7 +47,7 @@ async def get_filters_api():
     except Exception as e:
         logger.error("get_filters failed",exc_info=e)
 
-@router.post("/suppliers/get-suppliers-information")
+@router.post("/old/suppliers/get-suppliers-information")
 async def search_suppliers_get_suppliers_information_api_fun(apipostschema:SupplierInfoCountry):
     try:
         dbobj=database()
@@ -126,7 +126,7 @@ async def all_Suppliers_details(apipostschema:allSupplierDetails):
 
 
 
-@router.post("/v2/suppliers/get-supplier-information")
+@router.post("/suppliers/get-suppliers-information")
 async def get_supplier_infov2(apipostschema:SupplierInfoV2):
     try:
         logger.info(f"freetext:{apipostschema.text},region:{apipostschema.region},page_number:{apipostschema.page_number},page_size:{apipostschema.page_size},preferred_flag:{apipostschema.preferred_flag}")
