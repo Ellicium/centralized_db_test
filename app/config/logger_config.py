42ea3c9094ae7e0c4ede42fca735e68604f7c763
shahid.sayyed <shahid.sayyed@alixpartners.com>
2023-06-26 11:59:05 +0530

    added uvicorn and gunicorn loggers

--- app/config/logger_config.py
+++ app/config/logger_config.py
@@ -1,8 +1,15 @@
 import logging
 from fastapi.logger import logger
 
-def get_logger():
+def get_gunicorn_logger():
     gunicorn_logger = logging.getLogger('gunicorn.error')
     logger.handlers = gunicorn_logger.handlers
     logger.setLevel(gunicorn_logger.level)
+    return logger
+
+def get_uvicorn_logger():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    uvicorn_logger = logging.getLogger("uvicorn")
+    uvicorn_logger.propagate = False
+    logger = logging.getLogger(__name__)
     return logger
